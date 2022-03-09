86d05aa6841936d9b291b743185039af55896808
İlker Doğan <ilkerdogan1215@gmail.com>
2022-03-09 20:45:01 +0300

    Kayıt olma işlemi yapıldı.

---
+++ BusinessLayer/Concrete/RegisterManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class RegisterManager : IRegisterService
+    {
+        IUserDal _userDal;
+
+        public RegisterManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public void UserAdd(User user)
+        {
+            _userDal.Insert(user);
+        }
+    }
+}
---
+++ RecruitmentVideoWebProject/Controllers/RegisterController.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecruitmentVideoWebProject.Controllers
+{
+    public class RegisterController : Controller
+    {
+        RegisterManager userManager = new RegisterManager(new EfUserDal());
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UserRegister()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UserRegister(User u)
+        {
+            RegisterValidator registerValidator = new RegisterValidator();
+            ValidationResult results = registerValidator.Validate(u);
+            if (results.IsValid)
+            {
+                userManager.UserAdd(u);
+                return RedirectToAction("UserRegister", "Register");
+
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
