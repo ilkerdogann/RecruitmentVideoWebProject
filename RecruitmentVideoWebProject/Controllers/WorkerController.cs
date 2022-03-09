e882db2f9a56bfe6d1b99b11f21843948ba76eaa
İlker Doğan <ilkerdogan1215@gmail.com>
2022-03-09 20:46:15 +0300

    Diğer işlemler.

---
+++ RecruitmentVideoWebProject/Controllers/WorkerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecruitmentVideoWebProject.Controllers
+{
+    public class WorkerController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
