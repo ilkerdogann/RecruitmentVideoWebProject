2534b55d740af4d63fef659bc05944ddd0bdec25
İlker Doğan <ilkerdogan1215@gmail.com>
2022-03-09 20:44:18 +0300

    EntityLayer katmanının sınıfları oluşturuldu.

---
+++ EntityLayer/Concrete/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Manager
+    {
+        public int ManagerID { get; set; }
+        public string ManagerName { get; set; }
+        public string ManagerPassword { get; set; }
+        public string ManagerMail { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Worker
+    {
+        public int WorkerID { get; set; }
+        public string WorkerName { get; set; }
+        public string WorkerPassword { get; set; }
+        public string WorkerMail { get; set; }
+    }
+}
