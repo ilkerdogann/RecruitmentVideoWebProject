c81b61935e5391858f11e71afd7ff691cab848b8
İlker Doğan <ilkerdogan1215@gmail.com>
2022-03-12 14:27:05 +0300

    Yönetici işlemleri tamamlandı.

--- RecruitmentVideoWebProject/Controllers/ManagerController.cs
+++ RecruitmentVideoWebProject/Controllers/ManagerController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,12 @@ namespace RecruitmentVideoWebProject.Controllers
 {
     public class ManagerController : Controller
     {
-        
+        UserManager um = new UserManager(new EfUserDal());
+
         public ActionResult Index()
         {
-            return View();
+            var UserValues = um.GetList();
+            return View(UserValues);
         }
     }
 }
