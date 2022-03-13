6627ef34c3b56b103cd2e9f3ee2b9bf0baeb1ac4
İlker Doğan <ilkerdogan1215@gmail.com>
2022-03-13 15:44:08 +0300

    'Jitsi' projeye eklendi.

---
+++ RecruitmentVideoWebProject/Controllers/MeetController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecruitmentVideoWebProject.Controllers
+{
+    public class MeetController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
