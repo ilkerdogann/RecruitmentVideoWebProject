7733dd43c59beb775ce4d5905dbf41cb88b46566
İlker Doğan <ilkerdogan1215@gmail.com>
2022-03-14 17:37:45 +0300

    Kullanıcıların verileri için Rest API mimarisi oluşturuldu.

---
+++ RecruitmentVideoWebProject/Controllers/Api/UserApiController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using RecruitmentVideoWebProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace RecruitmentVideoWebProject.Controllers.Api
+{
+    public class UserApiController : Controller
+    {
+        Context c = new Context();
+        UserManager um = new UserManager(new EfUserDal());
+
+        public IHttpActionResult Get()
+        {
+            var model = c.Users.Select(s => new UserModel
+            {
+                UserID = s.UserID,
+                UserName = s.UserName,
+                UserMail = s.UserMail,
+                UserPassword = s.UserPassword,
+            }).ToList();
+            return Ok(model);
+        }
+
+        public IHttpActionResult Post(UserModel model)
+        {
+            var user = new User
+            {
+                UserID = model.UserID,
+                UserName = model.UserName,
+                UserMail = model.UserMail,
+                UserPassword = model.UserPassword,
+            };
+            um.UserAdd(user);
+            return Ok();
+        }
+
+        public IHttpActionResult Ok(List<UserModel> model)
+        {
+            return Ok(model);
+        }
+
+        public IHttpActionResult Ok()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ RecruitmentVideoWebProject/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecruitmentVideoWebProject.Models
+{
+    public class UserModel
+    {
+        public int UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserPassword { get; set; }
+
+        public string UserMail { get; set; }
+    }
+}
